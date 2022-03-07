d36af8dfeba61c8adee41ea77e5fc405f929219a
JellyBoonz <86168103+JellyBoonz@users.noreply.github.com>
2022-03-07 15:33:53 -0500

    Add files via upload

---
+++ Scripts/FallingBlock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Vector2 speedMinMax;
+    float speed;
+
+    private void Start()
+    {
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDifficultyPercent());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(transform.position);
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+    }
+}
